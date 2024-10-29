0f6ff94701f7ec805ead5528a84cc42786af39dd
maybejelle <72877973+maybejelle@users.noreply.github.com>
2024-10-29 14:11:11 +0100

    location deletion works

--- cms-front-end/src/components/LocationComponent.vue
+++ cms-front-end/src/components/LocationComponent.vue
@@ -1,12 +1,14 @@
 <template>
-    <div @click="navigateToOverview">
-        <h2>{{location.name}}</h2>
-        <button>Delete</button>
+    <div >
+        <div class="overview" @click="navigateToOverview"><h2>{{location.name}}</h2></div>
+        
+        <button @click="deleteLocation">Delete</button>
     </div>
 </template>
 
 <script>
 import { useRouter } from 'vue-router';
+import axios from 'axios';
 
 
 export default {
@@ -19,6 +21,23 @@ export default {
            router.push({path: `/device-overview/${props.location.documentId}`});
         }
         return { navigateToOverview }
+    },
+    methods: {
+        deleteLocation(){
+            console.log('delete location');
+            axios.delete(`http://localhost:8888/api/locations/${this.location.documentId}`,{
+                headers: {
+                    Authorization: `Bearer ${this.$cookies.get('jwt')}`
+                }
+            })
+                .then(response => {
+                    console.log(response);
+                    window.location.reload();
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
     }
 }
 </script>
@@ -48,5 +67,11 @@ button{
     cursor: pointer;
     font-size: 1.5rem;
     margin-right: 2rem;
+}
+
+.overview{
+    border: none;
+    padding: 0;
+    margin: 0;
 }
 </style>
