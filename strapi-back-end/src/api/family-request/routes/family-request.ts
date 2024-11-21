da48e6cbacb136021d4527de53a6a64a08f4d0fa
Bortier Jelle <jelle.bortier@student.howest.be>
2024-11-21 11:24:34 +0100

    added middleware

--- strapi-back-end/src/api/family-request/routes/family-request.ts
+++ strapi-back-end/src/api/family-request/routes/family-request.ts
@@ -4,4 +4,25 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::family-request.family-request');
+export default factories.createCoreRouter(
+  "api::family-request.family-request",
+  {
+    config: {
+      create: {
+        middlewares: [],
+      },
+      update: {
+        middlewares: ["global::is-recipient"],
+      },
+      delete: {
+        middlewares: ["global::is-recipient"],
+      },
+      find: {
+        middlewares: ["global::is-recipient"],
+      },
+      findOne: {
+        middlewares: ["global::is-recipient"],
+      },
+    },
+  }
+);
---
+++ strapi-back-end/src/middlewares/is-recipient.ts
@@ -0,0 +1,63 @@
+"use strict";
+
+module.exports = (config, { strapi }) => {
+  return async (ctx, next) => {
+    const entryId = ctx.params.id;
+    const user = ctx.state.user;
+    const userId = user?.id;
+
+    if (!userId) return ctx.unauthorized(`You can't access this entry`);
+
+    const apiName = ctx.state.route.info.apiName;
+    const appUid = generateUID(apiName);
+
+    if (entryId) {
+        const request = await getRequest(strapi, appUid, entryId);
+        console.log(request);
+        if (!isAuthorizedUser(request.recipient, userId, ctx)) return ctx.unauthorized(`You can't access this entry`);
+
+    } else {
+      addFiltersToQuery(ctx, userId);
+    }
+
+    await next();
+  };
+
+  function getRequest(strapi, appUid, entryId) {
+    return strapi.query(appUid).findOne({
+        select: ["documentId"],
+        where: { documentId: entryId },
+        populate: {recipient: true},
+        });
+    }
+
+  function generateUID(apiName) {
+    const apiUid = `api::${apiName}.${apiName}`;
+    return apiUid;
+  }
+
+ 
+
+
+
+
+  function isAuthorizedUser(user, userId, ctx) {
+    const isOwner = user.id === userId;
+    if (!isOwner) {
+      ctx.unauthorized(`You can't access this entry`);
+      return false;
+    }
+    return true;
+  }
+
+  function addFiltersToQuery(ctx, userId) {
+    ctx.query = {
+      ...ctx.query,
+      filters: {
+        ...ctx.query.filters,
+        $or: [{ recipient: userId },
+        ],
+      },
+    };
+  }
+};
