f0acb97ac475a14ee39103c40564ae01126a23b5
Bortier Jelle <jelle.bortier@student.howest.be>
2024-12-21 14:51:38 +0100

    help me

--- strapi-back-end/src/api/family/routes/family.ts
+++ strapi-back-end/src/api/family/routes/family.ts
@@ -4,4 +4,22 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::family.family');
+export default factories.createCoreRouter("api::family.family", {
+  config: {
+    create: {
+      middlewares: [],
+    },
+    update: {
+      middlewares: ["global::familyMiddleware"],
+    },
+    delete: {
+      middlewares: [],
+    },
+    find: {
+      middlewares: [],
+    },
+    findOne: {
+      middlewares: [],
+    },
+  },
+});
